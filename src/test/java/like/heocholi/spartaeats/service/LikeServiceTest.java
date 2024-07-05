952512fe9d3e14fa1f44280cb9741e998d67e32c
EugeneChoi <100894921+geneeuchoi@users.noreply.github.com>
2024-07-05 12:35:11 +0900

    TEST: 좋아요 단위 테스트 - 자신이 쓴 리뷰에 좋아요(예외처리 테스트)

--- src/test/java/like/heocholi/spartaeats/service/LikeServiceTest.java
+++ src/test/java/like/heocholi/spartaeats/service/LikeServiceTest.java
@@ -114,4 +114,35 @@ class LikeServiceTest {
         assertFalse(result);
     }
 
+    @Test
+    public void 자신이_쓴_리뷰에_좋아요() {
+        //given
+
+        //customer1 생성
+        SignupRequestDto signupRequestDto = new SignupRequestDto("test1234", "Test1234!!", "testName", "testAddress");
+        String encodedPassword = "testPassword";
+        Customer customer1 = new Customer(signupRequestDto, encodedPassword);
+
+        ReflectionTestUtils.setField(customer1, "id", 1L);
+
+        //customer1이 customer1의 리뷰에 좋아요 등록
+        Order order = new Order();
+        Store store = new Store();
+        String contents = "test Contents";
+        Review review1 = new Review(order, store, customer1, contents);
+
+        ReflectionTestUtils.setField(review1, "id", 1L);
+        Long review1Id = review1.getId();
+
+        // Mock 설정
+        given(reviewRepository.findById(review1Id)).willReturn(Optional.of(review1));
+
+        //when & then
+        LikeException exception = assertThrows(LikeException.class, () -> {
+            likeService.likeReview(review1Id, customer1);
+        });
+
+        assertEquals(ErrorType.INVALID_LIKE, exception.getErrorType());
+    }
+
 }
