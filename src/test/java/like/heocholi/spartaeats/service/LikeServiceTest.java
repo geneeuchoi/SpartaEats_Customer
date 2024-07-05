3fa6c42d778a8544475619385715498b5b6ecffa
EugeneChoi <100894921+geneeuchoi@users.noreply.github.com>
2024-07-05 12:34:44 +0900

    TEST: 좋아요 단위 테스트 - 좋아요 취소

--- src/test/java/like/heocholi/spartaeats/service/LikeServiceTest.java
+++ src/test/java/like/heocholi/spartaeats/service/LikeServiceTest.java
@@ -68,4 +68,50 @@ class LikeServiceTest {
         assertTrue(result);
     }
 
+    @Test
+    public void 좋아요_취소() {
+        //given
+
+        //customer1 생성
+        SignupRequestDto signupRequestDto = new SignupRequestDto("test1234", "Test1234!!", "testName", "testAddress");
+        String encodedPassword = "testPassword";
+        Customer customer1 = new Customer(signupRequestDto, encodedPassword);
+
+        //customer2 생성
+        SignupRequestDto signupRequestDto2 = new SignupRequestDto("test5678", "Test1234!!", "testName", "testAddress");
+        String encodedPassword2 = "testPassword";
+        Customer customer2 = new Customer(signupRequestDto2, encodedPassword2);
+
+        //id 설정
+        ReflectionTestUtils.setField(customer1, "id", 1L);
+        ReflectionTestUtils.setField(customer2, "id", 2L);
+
+        //리뷰 생성
+        Order order = new Order();
+        Store store = new Store();
+        String contents = "test Contents";
+        Review review1 = new Review(order, store, customer2, contents);
+
+        //리뷰 id 설정
+        ReflectionTestUtils.setField(review1, "id", 1L);
+        Long review1Id = review1.getId();
+
+        //Like 설정
+        Like like = Like.builder()
+                .customer(customer1)
+                .review(review1)
+                .isLike(true)
+                .build();
+
+        // Mock 설정
+        given(reviewRepository.findById(review1Id)).willReturn(Optional.of(review1));
+        given(likeRepository.findByCustomerIdAndReviewId(customer1.getId(), review1Id)).willReturn(Optional.of(like));
+
+        //when
+        boolean result = likeService.likeReview(review1Id, customer1);
+
+        //then
+        assertFalse(result);
+    }
+
 }
